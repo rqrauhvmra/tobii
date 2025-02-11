f5a12e6a4fdd9d6876ba522d504f5983a3fa45c7
François <f.mably@gmail.com>
2025-02-11 11:44:57 +0100

    Encapsulate counter text in P element for better accessibility compliance

--- src/js/index.js
+++ src/js/index.js
@@ -769,7 +769,7 @@ export default function Tobii (userOptions) {
    *
    */
   const updateCounter = () => {
-    counter.textContent = `${groups[activeGroup].currentIndex + 1}/${groups[activeGroup].elementsLength}`
+    counter.innerHTML = `<p>${groups[activeGroup].currentIndex + 1}/${groups[activeGroup].elementsLength}</p>`
   }
 
   /**

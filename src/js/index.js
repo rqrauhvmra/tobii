d2a822af9fa48e82ab45cb8df06e157c9acd92e8
François <f.mably@gmail.com>
2025-02-11 12:26:55 +0100

    Add new dialogTitle setting to allow dialog title customization for better accessibility compliance

--- src/js/index.js
+++ src/js/index.js
@@ -106,6 +106,7 @@ export default function Tobii (userOptions) {
       close: true,
       closeText: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><path stroke="none" d="M0 0h24v24H0z"/><line x1="18" y1="6" x2="6" y2="18" /><line x1="6" y1="6" x2="18" y2="18" /></svg>',
       closeLabel: 'Close lightbox',
+      dialogTitle: 'Lightbox',
       loadingIndicatorLabel: 'Image loading',
       counter: true,
       download: false, // TODO
@@ -377,7 +378,7 @@ export default function Tobii (userOptions) {
     lightbox.setAttribute('role', 'dialog')
     lightbox.setAttribute('aria-hidden', 'true')
     lightbox.setAttribute('aria-modal', 'true')
-    lightbox.setAttribute('aria-label', 'Lightbox')
+    lightbox.setAttribute('aria-label', userSettings.dialogTitle)
     lightbox.classList.add('tobii')
 
     // Adc theme class

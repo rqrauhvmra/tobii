8ccbd5ec618a6f6f18dd2891013133172a27ebd2
midzer <midzer@gmail.com>
2025-02-15 21:18:28 +0100

    fix unclickable top region for docClose

--- src/js/index.js
+++ src/js/index.js
@@ -863,7 +863,7 @@ export default function Tobii (userOptions) {
     } else if (event.target === nextButton) {
       next()
     } else if (event.target === closeButton ||
-      (event.target.classList.contains('tobii__slide') && userSettings.docClose)) {
+      (event.target.classList.contains('tobii__slide') || event.target.classList.contains('tobii') && userSettings.docClose)) {
       close()
     }
 
